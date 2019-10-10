9a0239a32f622fefd5b8cb0e00449e90c516f97f
chris <zhengjin3@jd.com>
2019-10-10 17:31:49 +0800

    clear file

--- examples/more.js
+++
@@ -1,9 +0,0 @@
-import 'rc-pagination/assets/index.less';
-import Pagination from 'rc-pagination';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <Pagination className="ant-pagination" defaultCurrent={3} total={450} />,
-  document.getElementById('__react-content')
-);
--- examples/styles.js
+++
@@ -1,9 +0,0 @@
-import 'rc-pagination/assets/index.less';
-import Pagination from 'rc-pagination';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <Pagination defaultCurrent={2} total={25} style={{ margin: '100px' }} />,
-  document.getElementById('__react-content')
-);
